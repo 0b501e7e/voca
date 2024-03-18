7f96ab7f177cdb31629f56b2cbefe2b1627e8b5f
@warnocs2 <senan.warnock2@mail.dcu.ie>
2024-03-18 22:11:47 +0000

    add snarkjs dependency, remove docker files

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script expects the name of the circuit directory as the first argument
-CIRCUIT_DIR=$1
-
-# Navigate to the specified circuit directory and run its test.sh script
-cd /app/circuits/$CIRCUIT_DIR
-./test.sh
