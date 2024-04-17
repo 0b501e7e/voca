b52c0d6de479bae8988e146ba035afc9b55f4641
@warnocs2 <senan.warnock2@mail.dcu.ie>
2024-04-17 07:43:39 +0100

    chore: forge init

---
+++ src/circuits/generate_withdraw.js
@@ -0,0 +1,37 @@
+const snarkjs = require("snarkjs");
+const fs = require("fs");
+const { eddsa, poseidon } = require("circomlibjs");
+const {stringifyBigInts, unstringifyBigInts} = require('./utils/stringifybigint.js')
+
+var prvKey = Buffer.from("4".padStart(64,'0'), "hex");
+
+var pubKey = eddsa.prv2pub(prvKey);
+
+var nonce = 0;
+var txRoot = BigInt('14053325031894235002744541221369412510941171790893507881802249870625790656164')
+var recipient = BigInt('0xC33Bdb8051D6d2002c0D80A1Dd23A1c9d9FC26E4');
+var m = poseidon([nonce, recipient])
+
+var signature = eddsa.signPoseidon(prvKey, m);
+
+var verify = eddsa.verifyPoseidon(m, signature, pubKey)
+console.log(verify)
+
+const inputs = {
+    Ax: stringifyBigInts(pubKey[0]),
+    Ay: stringifyBigInts(pubKey[1]),
+    R8x: stringifyBigInts(signature.R8[0]),
+    R8y: stringifyBigInts(signature.R8[1]),
+    S: stringifyBigInts(signature.S),
+    M: stringifyBigInts(m)
+}
+
+fs.writeFileSync(
+    "input_withdraw.json",
+    JSON.stringify(inputs, (key, value) => 
+        typeof value === 'bigint'
+            ? value.toString() 
+            : value // return everything else unchanged
+    ),
+    "utf-8"
+);
